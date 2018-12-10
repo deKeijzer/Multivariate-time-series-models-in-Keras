dcb27b277655694bcd644bac6d61f8da3af31c37
deKeijzer <16011015@student.hhs.nl>
2018-12-10 14:13:38 +0100

    Basic environment created.

---
+++ notebooks/keijzer.py
@@ -0,0 +1,429 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
+
+def setup_multi_gpus():
+    """
+    Setup multi GPU usage
+
+    Example usage:
+    model = Sequential()
+    ...
+    multi_model = multi_gpu_model(model, gpus=num_gpu)
+    multi_model.fit()
+
+    About memory usage:
+    https://stackoverflow.com/questions/34199233/how-to-prevent-tensorflow-from-allocating-the-totality-of-a-gpu-memory
+    """
+    import tensorflow as tf
+    from keras.utils.training_utils import multi_gpu_model
+    from tensorflow.python.client import device_lib
+
+    # IMPORTANT: Tells tf to not occupy a specific amount of memory
+    from keras.backend.tensorflow_backend import set_session  
+    config = tf.ConfigProto()  
+    config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU  
+    sess = tf.Session(config=config)  
+    set_session(sess)  # set this TensorFlow session as the default session for Keras.
+
+    
+    # getting the number of GPUs 
+    def get_available_gpus():
+       local_device_protos = device_lib.list_local_devices()
+       return [x.name for x in local_device_protos if x.device_type    == 'GPU']
+    
+    num_gpu = len(get_available_gpus())
+    print('Amount of GPUs available: %s' % num_gpu)
+    
+    return num_gpu
+
+
+def create_corr_matrix(df, dwelling_id, annot, size=(25,25)):
+    """
+    Pearson correlation coefficient matrix. 
+    The Pearson correlation coefficient is a measure of the linear correlation between two variables.
+    """
+    plt.clf()
+    
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+
+    if annot:
+        fig, ax = plt.subplots(figsize=size)
+    else:
+        fig, ax = plt.subplots(figsize=size)
+    
+    fig = sns.heatmap(corr, mask=mask, square=False, cmap='RdYlGn', annot=annot, ax=ax, 
+                cbar_kws={'label':'Pearson correlation coefficient [-]'})
+
+    fig.set_title('Correlation matrix of dwelling ID: '+dwelling_id)
+    fig.tick_params(axis='x', rotation=90)
+    fig.tick_params(axis='y', rotation=0)
+
+    fig = fig.get_figure()
+    fig.tight_layout()
+    fig.show()
+    
+    print('Saving heatmap')
+    #fig.savefig('//datc//opschaler//EDA//Pearson_corr//' + dwelling_id + '.png', dpi=300)
+    return fig
+
+
+def reduce_memory(df):
+    """
+    Reduces memory footprint of the input dataframe.
+    Changes float64 columns to float32 dtype.
+    """
+    columns = df.columns
+    memory_before = df.memory_usage(deep=False).sum() / 2**30 # convert bytes to GB
+
+    for column in tqdm(columns):
+        if df[column].dtype == 'float64':
+            df[column] = df[column].astype('float32')
+        
+    memory_after = df.memory_usage(deep=False).sum() / 2**30 # convert bytes to GB
+    print('Memory uasge reduced from %.3f GB to %.3f GB' % (memory_before, memory_after))
+    
+    return df
+
+
+def resample_df(df, sample_rate, combine_all_dwellings=False):
+    """
+    Resampled a (un)processed dataframe to the specified sample_rate.
+    Input is a (un)processed df.
+    Input df may also be multiple dwelling dfs combined.
+    Sample rate must be a string. 
+    For example '1H', '1D', '60s'.
+    
+    Combine all dwellings: resamples the df and ignores the fact that there are unique dwellings.
+    
+    TODO: add std to ePower, gasPower when combine_all_dwellings=False
+    """
+    df = df.copy()
+    
+    def resample_dwelling(df, sample_rate, dwelling_id):
+        new_df = df.resample(sample_rate).mean() # resample to rest by mean
+        new_df['dwelling'] = dwelling_id
+        if combine_all_dwellings:
+            new_df['gasPower_std'] = df['gasPower'].resample(sample_rate).std() # gives the std (for example that day)
+        return new_df
+        
+                      
+    resampled_dwellings = []
+    
+    if combine_all_dwellings: # Ignore dwelling_ids
+        df = df.drop(['eMeter', 'eMeterReturn', 'eMeterLow', 'eMeterLowReturn', 'gasMeter'], axis=1) # Drop columns because they are meaningless when ignoring dwelling ids
+        resampled_df = resample_dwelling(df, sample_rate, 'All dwellings')
+        resampled_dwellings.append(resampled_df)
+    else:
+        dwellings = df['dwelling'].unique() # Get dwelling ids
+        for dwelling_id in tqdm(dwellings):
+            dwelling_df = df[df['dwelling'] == dwelling_id] # Get the data from only that dwelling_id
+            resampled_dwelling = resample_dwelling(dwelling_df, sample_rate, dwelling_id)
+            resampled_dwellings.append(resampled_dwelling)
+    
+    resampled_df = pd.concat(resampled_dwellings)
+    
+    return resampled_df
+
+
+def abs_percentage_error(y_true, y_pred):
+    import keras.backend as K
+    """
+    Returns the absolute value of the difference between y_true and y_pred (in percentage).
+    For examples on losses see:
+    https://github.com/keras-team/keras/blob/master/keras/losses.py
+    """
+    return (K.abs(y_true - y_pred) / K.abs(y_pred)) * 100
+
+
+def create_timeseries_history(df, columns_to_lookback, n_lookback, dropnan=True):
+    """
+    df, pandas dataframe
+    columns_to_lookback, the columns to gather historical data from
+    n_lookback, amount of samples to look back for. 
+    
+    Example:
+    df = dwelling_df['FF']
+    columns_to_lookback = df.columns
+    n_lookback = 3
+    
+    
+    Output columns:
+    FF, FF (t-1), FF (t-2), FF (t-3)
+    
+    Where FF (t-3) contains the FF value of 3 samples (indices) back.
+    """
+    n_lookback += 1 # +1 because iteration starts at 0.
+    df = df.copy()
+    
+    for column in columns_to_lookback:
+        if column == 'datetime':
+            pass
+        else:
+            for dt in range(n_lookback):
+                if dt == 0:
+                    pass
+                else: 
+                    df[column+'(t-%s)' % dt ] = df[column].shift(dt)
+    if dropnan:
+        df = df.dropna()
+        
+    return df
+
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    columns = data.columns
+    
+    n_vars = 1 if type(data) is list else data.shape[1]
+    dff = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(dff.shift(i))
+        names += [('%s(t-%d)' % (columns[j], i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(dff.shift(-i))
+        if i == 0:
+            names += [('%s(t)' % (columns[j])) for j in range(n_vars)]
+        else:
+            names += [('%s(t+%d)' % (columns[j], i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+def df_to_lstm_format(df, test_size=0.5, look_back=5, target_column='target', scale_X=True):
+    """
+    TODO: output train and test datetime
+    Input is a Pandas DataFrame. 
+    Output is a np array in the format of (samples, timesteps, features).
+    Currently this function only accepts one target variable.
+
+    Usage example:
+
+    # variables
+    df = data # should be a pandas dataframe
+    test_size = 0.5 # percentage to use for training
+    target_column = 'c' # target column name, all other columns are taken as features
+    scale_X = False
+    look_back = 5 # Amount of previous X values to look at when predicting the current y value
+    """
+    df = df.copy()
+
+    # Make sure the target column is the last column in the dataframe
+    df['target'] = df[target_column] # Make a copy of the target column
+    df = df.drop(columns=[target_column]) # Drop the original target column
+    
+    target_location = df.shape[1] - 1 # column index number of target
+    split_index = int(df.shape[0]*test_size) # the index at which to split df into train and test
+    
+    # ...train
+    X_train = df.values[:split_index, :target_location]
+    y_train = df.values[:split_index, target_location]
+
+    # ...test
+    X_test = df.values[split_index:, :target_location] # original is split_index:-1
+    y_test = df.values[split_index:, target_location] # original is split_index:-1
+
+    # Scale the features
+    if scale_X:
+        scalerX = StandardScaler(with_mean=True, with_std=True).fit(X_train)
+        X_train = scalerX.transform(X_train)
+        X_test = scalerX.transform(X_test)
+        
+    # Reshape the arrays
+    samples = len(X_train) # in this case 217 samples in the training set
+    num_features = target_location # All columns before the target column are features
+
+    samples_train = X_train.shape[0] - look_back
+    X_train_reshaped = np.zeros((samples_train, look_back, num_features))
+    y_train_reshaped = np.zeros((samples_train))
+
+    for i in range(samples_train):
+        y_position = i + look_back
+        X_train_reshaped[i] = X_train[i:y_position]
+        y_train_reshaped[i] = y_train[y_position]
+
+
+    samples_test = X_test.shape[0] - look_back
+    X_test_reshaped = np.zeros((samples_test, look_back, num_features))
+    y_test_reshaped = np.zeros((samples_test))
+
+    for i in range(samples_test):
+        y_position = i + look_back
+        X_test_reshaped[i] = X_test[i:y_position]
+        y_test_reshaped[i] = y_test[y_position]
+    
+    return X_train_reshaped, y_train_reshaped, X_test_reshaped, y_test_reshaped
+
+from keras.callbacks import Callback
+import keras.backend as K
+
+class LR_Finder(Callback):
+    """
+    This function is NOT made by me.
+    Source: http://puzzlemusa.com/2018/05/14/learning-rate-finder-using-keras/
+    """
+    def __init__(self, start_lr=1e-5, end_lr=10, step_size=None, beta=.98):
+        super().__init__()
+        
+        self.start_lr = start_lr
+        self.end_lr = end_lr
+        self.step_size = step_size
+        self.beta = beta
+        self.lr_mult = (end_lr/start_lr)**(1/step_size)
+        
+    def on_train_begin(self, logs=None):
+        self.best_loss = 1e9
+        self.avg_loss = 0
+        self.losses, self.smoothed_losses, self.lrs, self.iterations = [], [], [], []
+        self.iteration = 0
+        logs = logs or {}
+        K.set_value(self.model.optimizer.lr, self.start_lr)
+        
+    def on_batch_end(self, epoch, logs=None):
+        logs = logs or {}
+        loss = logs.get('loss')
+        self.iteration += 1
+        
+        self.avg_loss = self.beta * self.avg_loss + (1 - self.beta) * loss
+        smoothed_loss = self.avg_loss / (1 - self.beta**self.iteration)
+        
+        # Check if the loss is not exploding
+        if self.iteration>1 and smoothed_loss > self.best_loss * 4:
+            self.model.stop_training = True
+            return
+
+        if smoothed_loss < self.best_loss or self.iteration==1:
+            self.best_loss = smoothed_loss
+        
+        lr = self.start_lr * (self.lr_mult**self.iteration)
+        
+        self.losses.append(loss)
+        self.smoothed_losses.append(smoothed_loss)
+        self.lrs.append(lr)
+        self.iterations.append(self.iteration)
+        
+        
+        K.set_value(self.model.optimizer.lr, lr)  
+        
+    def plot_lr(self):
+        plt.xlabel('Iterations')
+        plt.ylabel('Learning rate')
+        plt.plot(self.iterations, self.lrs)
+        
+    def plot(self, n_skip=10):
+        plt.ylabel('Loss')
+        plt.xlabel('Learning rate (log scale)')
+        plt.plot(self.lrs[n_skip:-5], self.losses[n_skip:-5])
+        plt.xscale('log')
+        
+    def plot_smoothed_loss(self, n_skip=10):
+        plt.ylabel('Smoothed Losses')
+        plt.xlabel('Learning rate (log scale)')
+        plt.plot(self.lrs[n_skip:-5], self.smoothed_losses[n_skip:-5])
+        plt.xscale('log')
+        
+    def plot_loss(self):
+        plt.ylabel('Losses')
+        plt.xlabel('Iterations')
+        plt.plot(self.iterations[10:], self.losses[10:])
+        
+from keras.callbacks import Callback
+import keras.backend as K
+import numpy as np
+
+class SGDRScheduler(Callback):
+    """
+    Note that this code is NOT made by me. 
+    Source: https://www.jeremyjordan.me/nn-learning-rate/
+    """
+    '''Cosine annealing learning rate scheduler with periodic restarts.
+    # Usage
+        ```python
+            schedule = SGDRScheduler(min_lr=1e-5,
+                                     max_lr=1e-2,
+                                     steps_per_epoch=np.ceil(epoch_size/batch_size),
+                                     lr_decay=0.9,
+                                     cycle_length=5,
+                                     mult_factor=1.5)
+            model.fit(X_train, Y_train, epochs=100, callbacks=[schedule])
+        ```
+    # Arguments
+        min_lr: The lower bound of the learning rate range for the experiment.
+        max_lr: The upper bound of the learning rate range for the experiment.
+        steps_per_epoch: Number of mini-batches in the dataset. Calculated as `np.ceil(epoch_size/batch_size)`. 
+        lr_decay: Reduce the max_lr after the completion of each cycle.
+                  Ex. To reduce the max_lr by 20% after each cycle, set this value to 0.8.
+        cycle_length: Initial number of epochs in a cycle.
+        mult_factor: Scale epochs_to_restart after each full cycle completion.
+    # References
+        Blog post: jeremyjordan.me/nn-learning-rate
+        Original paper: http://arxiv.org/abs/1608.03983
+    '''
+    def __init__(self,
+                 min_lr,
+                 max_lr,
+                 steps_per_epoch,
+                 lr_decay=1,
+                 cycle_length=10,
+                 mult_factor=2):
+
+        self.min_lr = min_lr
+        self.max_lr = max_lr
+        self.lr_decay = lr_decay
+
+        self.batch_since_restart = 0
+        self.next_restart = cycle_length
+
+        self.steps_per_epoch = steps_per_epoch
+
+        self.cycle_length = cycle_length
+        self.mult_factor = mult_factor
+
+        self.history = {}
+
+    def clr(self):
+        '''Calculate the learning rate.'''
+        fraction_to_restart = self.batch_since_restart / (self.steps_per_epoch * self.cycle_length)
+        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (1 + np.cos(fraction_to_restart * np.pi))
+        return lr
+
+    def on_train_begin(self, logs={}):
+        '''Initialize the learning rate to the minimum value at the start of training.'''
+        logs = logs or {}
+        K.set_value(self.model.optimizer.lr, self.max_lr)
+
+    def on_batch_end(self, batch, logs={}):
+        '''Record previous batch statistics and update the learning rate.'''
+        logs = logs or {}
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+
+        self.batch_since_restart += 1
+        K.set_value(self.model.optimizer.lr, self.clr())
+
+    def on_epoch_end(self, epoch, logs={}):
+        '''Check for end of current cycle, apply restarts when necessary.'''
+        if epoch + 1 == self.next_restart:
+            self.batch_since_restart = 0
+            self.cycle_length = np.ceil(self.cycle_length * self.mult_factor)
+            self.next_restart += self.cycle_length
+            self.max_lr *= self.lr_decay
+            self.best_weights = self.model.get_weights()
+
+    def on_train_end(self, logs={}):
+        '''Set weights to the values from the end of the most recent cycle for best performance.'''
+        self.model.set_weights(self.best_weights)
